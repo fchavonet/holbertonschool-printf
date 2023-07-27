be261b286581d203da2ff1d3bb556608ae1d6411
Fabien Chavonet <fchavonet@mailo.com>
2023-07-27 10:25:06 -0700

    fix: specifiers_handler file upload forgotten in previous commit

---
+++ specifiers_handler.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * specifiers_handler - find the appropriate print function for the specifier
+ *
+ * @specifier: the conversion specifier to find the handler for
+ * @arguments: the va_list containing the arguments
+ *
+ * Return: the number of characters printed by the handler function (2)
+ */
+int specifiers_handler(const char specifier, va_list arguments)
+{
+	int index_array = 0;
+
+	format_handler array[] = {
+		{'c', print_character},
+		{'s', print_string},
+		{'%', print_percentage},
+		{'\0', NULL}
+	};
+
+	while (array[index_array].specifier != '\0')
+	{
+		if (specifier == array[index_array].specifier)
+		{
+			return (array[index_array].function(arguments));
+		}
+
+		index_array++;
+	}
+
+	_putchar('%');
+	_putchar(specifier);
+
+	return (2);
+}
