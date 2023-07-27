7f9eb5ffded34dd7660e076cfaed6f247954c591
Fabien Chavonet <fchavonet@mailo.com>
2023-07-27 09:37:44 -0700

    creation of the main _printf function

--- _printf.c
+++ _printf.c
@@ -24,3 +24,36 @@ int specifiers_handler(const char specifier, va_list arguments)
 
 	return (2);
 }
+
+int _printf(const char *format, ...)
+{
+	int index_format = 0;
+	int total_length = 0;
+
+	va_list arguments;
+
+	va_start(arguments, format);
+
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	{
+		return (-1);
+	}
+
+	while (format[index_format] != '\0')
+	{
+		if (format[index_format] == '%')
+		{
+			total_length += specifiers_handler(format[index_format + 1], arguments);
+			index_format += 2;
+		}
+		else
+		{
+			total_length += _putchar(format[index_format]);
+			index_format++;
+		}
+	}
+
+	va_end(arguments);
+
+	return (total_length);
+}
